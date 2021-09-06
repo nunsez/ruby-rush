a09c817f6236f0651a9ca3f013aa7757750d7e59
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-09-06 18:04:46 +0700

    add defined.rb

---
+++ rubytut2/lesson04/defined.rb
@@ -0,0 +1,13 @@
+a = 'defined'
+
+if defined?(a)
+    puts "Переменная 'a' определена"
+else
+    puts "Переменная 'a' не определена"
+end
+
+if defined?(b)
+    puts "Переменная 'b' определена"
+else
+    puts "Переменная 'b' не определена"
+end
