22898db0dacd6af5c762e7792b9ec9d9788de0cf
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-09-06 18:05:07 +0700

    add scopes.rb

---
+++ rubytut2/lesson04/scopes.rb
@@ -0,0 +1,21 @@
+$a = 'global'
+b = 'local'
+
+def method
+    c = 'method local'
+
+    puts "Переменная '$a': #{defined?($a)}"
+    puts "Переменная 'b': #{defined?(b)}"
+    puts "Переменная 'c': #{defined?(c)}"
+end
+
+puts 'inside method:'
+method
+puts
+
+puts 'top level:'
+puts "Переменная '$a': #{defined?($a)}"
+puts "Переменная 'b': #{defined?(b)}"
+puts "Переменная 'c': #{defined?(c)}"
+
+
