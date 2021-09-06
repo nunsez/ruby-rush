59a40e569cdd760c8cbc3c6c89f7e82813647161
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-09-06 16:35:59 +0700

    add roll_some_dice.rb

---
+++ rubytut2/lesson02/roll_some_dice.rb
@@ -0,0 +1,24 @@
+def show_rolling_die(range)
+    100.times do
+        print "#{rand(range)}\r"
+        sleep 0.01
+    end
+end
+
+RANGE = (1..6).freeze
+
+puts 'How many dice?'
+number = gets.to_i
+puts
+
+sum = 0
+
+number.times do
+    die = rand(RANGE)
+    sum += die
+
+    show_rolling_die(RANGE)
+    puts die
+end
+
+puts "Sum of dice: #{sum}"
